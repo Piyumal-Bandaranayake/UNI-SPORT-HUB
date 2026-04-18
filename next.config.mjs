b219bff612c79f47b23e1341d20dba0a79d6d11c
Piyumal Bandaranayake <piyumalbandaranayake24790@gmail.com>
2026-04-18 13:54:21 +0530

    feat: enable legacy peer dependencies and configure Next.js image remote patterns

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
   reactCompiler: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "res.cloudinary.com",
+      },
+      {
+        protocol: "https",
+        hostname: "lh3.googleusercontent.com", // Google profile pics (if needed later)
+      },
+    ],
+  },
 };
 
 export default nextConfig;
